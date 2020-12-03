6c1e1eee6d15410d65fdff59eab1fa1885a6fb97
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2020-12-03 11:26:18 +0100

    exo 77

--- exo 3/exercice3.js
+++ exo 3/exercice3.js
@@ -1,7 +1,11 @@
 /* Voici un exemple de fonction retournant du texte, inspirez vous de cet exemple pour réaliser l'exercice */
 
-function txtReturn() {
-    return "monTexte";
+function txtInsert() {
+    return "J'aime le Javascript";
+}
+
+function backgroundSet(){
+    return "magenta"
 }
 
 
--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,10 +1,13 @@
 /* Ecrire ci dessous une fonction permettant d'ajouter du texte dans le div appelé container */
-
+function text(){
+    let container =document.getElementById("container")
+    container.innerHTML = "Mon nouveaux texte";
+}
 
 
 /* Appeler la fonction */
 
 
-
+text();
 
 
